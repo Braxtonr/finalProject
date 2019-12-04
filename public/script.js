dc92faeb6e0570b9ddacd41fc35aa8082db65a13
braxtonr <braxtonrobinson9@gmail.com>
2019-12-04 13:54:50 -0500

    finalProj

--- public/script.js
+++ public/script.js
@@ -2,9 +2,9 @@ async function showTeams(){
     
     let response = await fetch(`api/nba-league/`);
     let teams = await response.json();
-    let leagueDiv = document.getElementById("league");
-    leagueDiv.innerHTML = "";
+    let leagueDiv = document.getElementById("content");
     console.log(teams);
+    leagueDiv.innerHTML = "";
     for(i in teams){
         leagueDiv.appendChild(getTeamElem(teams[i]));
     }
@@ -20,34 +20,70 @@ function getTeamElem(team){
     teamHeader.innerHTML = team.name;
     teamListDiv.append(teamHeader);
     teamPara = document.createElement('p');
-    teamPara.innerHTML = "Located in" + team.location + ". Representing the " + team.conference + " conference in the " 
+    teamPara.innerHTML = "Located in " + team.location + ". Representing the " + team.conference + " conference in the " 
     + team.arena + " arena. The team colors are " + team.colors + ".";
     teamListDiv.appendChild(teamPara);
+    let dltBtn = document.createElement("button");
+    dltBtn.innerHTML = "Delete";
+    dltBtn.onclick = deleteTeam;
+    dltBtn.setAttribute("data-id", team._id);
+    teamListDiv.append(dltBtn);
+    let editBtn = document.createElement("button");
+    editBtn.innerHTML = "Edit";
+    editBtn.onclick = editTeam
+    editBtn.setAttribute("data-id", team._id);
+    teamListDiv.append(editBtn);
     return teamDiv;
 }
 
-function showForm(){
+async function editForm(){
+    let teamId = this.getAttribute("data-id");
+    document.getElementById("edit-team-id").textContent = teamId;
+
+    let response = await fetch(`api/nba-league/${teamId}`);
+
+    if(response.status != 200){
+        return;
+    }
+
+    let team = await response.json();
+    document.getElementById('edit-team-name').value = team.name;
+    document.getElementById("edit-team-location").value= team.location;
+    document.getElementById("edit-team-conference").value= team.conference;
+    document.getElementById("edit-team-arena").value = team.arena;
+    
+    if(team.colors != null){
+        document.getElementById("edit-team-colors").value = team.colors.join('\n');
+    }
+}
+function showAddForm(){
     let teamForm = document.getElementById("add-team");
-    teamForm.classList.remove("hidden");
+    teamForm.classList.toggle("hidden");
 }
 
+function clearForm(){
+    document.getElementById("team-name").value = "";
+    document.getElementById("team-location").value = "";
+    document.getElementById("team-conference").value = "";
+    document.getElementById("team-arena").value = "";
+    document.getElementById("team-colors").value = "";
+}
 
 
 async function addTeam(){
     let teamForm = document.getElementById("add-team");
-    const name = document.getElementById("team-name").value;
-    const location = document.getElementById("team-location").value;
-    const conference = document.getElementById("team-conference").value;
-    const arena = document.getElementById("team-arena").value;
-    const directionsText = document.getElementById("team-colors").value;
+    const name = document.getElementById("team-name").value.trim();
+    const location = document.getElementById("team-location").value.trim();
+    const conference = document.getElementById("team-conference").value.trim();
+    const arena = document.getElementById("team-arena").value.trim();
+    const directionsText = document.getElementById("team-colors").value.trim();
     const colors = directionsText.split("\n");
-    const feedbackP = document.getElementById("feedback");
-    feedbackP.classList.remove("error");
-    feedbackP.classList.remove("success");
-    feedbackP.classList.remove("hidden");
+    const message = document.getElementById("feedback");
+    message.classList.remove("error");
+    message.classList.remove("success");
+    message.classList.remove("hidden");
 
     let team = {"name": name, "location": location, "conference": conference, "arena": arena, "colors":colors};
-    console.log(team);
 
     let response = await fetch('/api/nba-league/', {
         method: 'POST',
@@ -58,21 +94,64 @@ async function addTeam(){
     });
 
     if(response.status != 200){
-        feedbackP.innerHTML = "Error Adding Team to League";
-        feedbackP.classList.add("error");
-        feedbackP.classList.add("hidden");
+        message.innerHTML = "Error";
+        message.classList.add("error");
+        message.classList.add("hidden");
         return;
     }
 
     let result = await response.json();
-    feedbackP.innerHTML = "Successfully Added Team to League";
-    feedbackP.classList.add("success"); 
-    teamForm.classList.add("hidden"); 
-    showTeams();  
+    message.innerHTML = "Successful";
+    message.classList.add("success"); 
+    setTimeout(function(){ message.classList.add("hidden"); }, 1500);
+    setTimeout(function(){ teamForm.classList.add("hidden");} ,1500);
+    showTeams();
+    clearForm();
+}
+
+async function editTeam(){
+    let id = document.getElementById("edit-workout-id").textContent;
+    let name = document.getElementById("edit-team-name").value;
+    let location = document.getElementById("edit-team-location").value;
+    let conference = document.getElementById("edit-team-conference").value;
+    let arena = document.getElementById("edit-team-arena").value;
+    let colors = document.getElementById("edit-team-colors").value;
+    let team = {"name":name, "location": location, "conference": conference, "arena": arena, "colors": colors};
+
+    let response = await fetch(`api/nba-league/${id}`, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(team),
+    });
+
+    if(response.status != 200){
+        console.log("Error editing team");
+    }
+    let result = await response.json;
+    showTeams();
+}
+
+async function deleteTeam(){
+    let teamId = this.getAttribute('data-id');
+
+    let response = await fetch(`/api/nba-league/${teamId}`, {
+        method: 'DELETE',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        }
+    });
+    if(response.status != 200){
+        console.log("Error deleting team");
+        return;
+    }
+    
+    let result = await response.json();
+    showTeams();
 }
 
 window.onload = function(){
-    this.document.getElementById("show-form").onclick = showForm;
     this.document.getElementById("btn-add-team").onclick = addTeam;
-    this.showTeams();
+    showTeams();
 }
--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const teamSchema = new mongoose.Schema({
     colors:[String]
 });
 
-
 const Team =  mongoose.model('Team', teamSchema);
 
 async function createTeam(team){
@@ -57,23 +56,65 @@ app.post('/api/nba-league', (req,res)=>{
     res.send(team);
 });
 
-
-
-//render our html page
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
-
 async function getTeams(res){
     const teams = await Team.find();
-    console.log("First" + teams);
+    console.log(teams);
     res.send(teams);
 }
 
 app.get('/api/nba-league', (req,res)=>{
-    const teams = getTeams(res);
+    let teams = getTeams(res);
 });
+
+app.get('/api/nba-league/:id',(req,res)=>{
+    let team = getTeam(req.params.id,res)
+})
+
+async function getTeam(id,res){
+    const team = await Team
+    .findOne({_id:id});
+    console.log(team);
+    res.send(team);
+}
+
+app.put('/api/nba-league/:id',(req,res)=>{
+    const result = validateTeam(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    updateTeam(res,req.params.id, req.body.name, req.body.location, req.body.conference, req.body.arena, req.body.colors );
+});
+
+async function updateTeam(res, id, name, location, conference, arena, colors) {
+    //fist param: to find, second update
+    const result = await Team.updateOne({_id:id},{
+        $set:{
+            name: name,
+            location: location,
+            conference: conference,
+            arena:arena,
+            colors:colors
+        }
+    })
+    
+    res.send(result);
+}
+
+app.delete('/api/nba-league/:id',(req,res)=>{
+    removeTeam(res, req.params.id);
+});
+
+async function removeTeam(res, id) {
+    const team = await Team.findByIdAndRemove(id);
+    res.send(team);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
